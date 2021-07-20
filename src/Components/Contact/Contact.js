f9dbd752c19cfc467d43e134109f1bd5ac0f4a3b
Habib <heirs.qih@gmail.com>
2021-07-20 15:52:02 -0700

    delete changed in contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
           <p className="lead">Here are my contact details </p>
           <p className="lead">Email : khabibullosf@gmail.com my email </p>
           <p className="lead">Phone : 415 323 8313 </p>
-          <p className="lead">I will available morning 9AM to 9PM Pacific Time via slack or whatsapp Changed  </p>
+          <p className="lead">I will available morning 9AM to 9PM Pacific Time via slack or whatsapp</p>
         </Container>
       </Jumbotron>
     </div>
